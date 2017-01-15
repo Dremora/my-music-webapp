52b72b7d505fac6e5a9325d866ec57d1ba75c517
Kirill Korolyov <kirill.korolyov@gmail.com>
2017-01-15 10:50:11 +0000

    Add redux-devtools

--- src/index.js
+++ src/index.js
@@ -3,25 +3,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import Router from 'react-router/BrowserRouter';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { createDevTools } from 'redux-devtools';
+import LogMonitor from 'redux-devtools-log-monitor';
+import DockMonitor from 'redux-devtools-dock-monitor';
 
 import todoApp from './reducers';
 import saga from './sagas';
 import App from './App';
 import './index.css';
 
+const DevTools = createDevTools(
+  (
+    <DockMonitor
+      toggleVisibilityKey="ctrl-h"
+      changePositionKey="ctrl-q"
+      defaultIsVisible={false}
+    >
+      <LogMonitor />
+    </DockMonitor>
+  )
+);
+
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(todoApp, applyMiddleware(sagaMiddleware));
+
+const enhancer = compose(
+  applyMiddleware(sagaMiddleware),
+  DevTools.instrument()
+);
+const store = createStore(todoApp, enhancer);
 
 sagaMiddleware.run(saga);
 
 ReactDOM.render(
   (
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <div>
+        <Router>
+          <App />
+        </Router>
+        <DevTools />
+      </div>
     </Provider>
   ),
   document.getElementById('root')
