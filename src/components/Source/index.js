a2ad2078717586fa1b9cc8db887a8a918b8d2996
Kirill Korolyov <kirill.korolyov@gmail.com>
2018-01-23 03:07:03 +0000

    Use new components in forms

--- src/components/Source/index.js
+++ src/components/Source/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { Field } from 'formik';
 
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 import Textarea from '../../components/Textarea';
+import FormField from '../../components/FormField';
+import Button from '../../components/Button';
 
 const locations = [
   { id: 'APPLE_MUSIC', label: 'Apple Music' },
@@ -23,41 +25,57 @@ const formats = [
 ];
 
 export default ({ source, i, onRemove }) => (
-  <div>
-    <Field placeholder="Location" name={`sources.${i}.location`} component={Select}>
-      {locations.map(location => (
-        <option value={location.id} key={location.id}>
-          {location.label}
-        </option>
-      ))}
-    </Field>
-    <Field placeholder="MBID" name={`sources.${i}.mbid`} component={Input} />
-    <Field placeholder="Comments" name={`sources.${i}.comments`} component={Textarea} />
-    {source.location !== 'SPOTIFY' && (
-      <Field placeholder="Tag issues" name={`sources.${i}.tagIssues`} component={Input} />
-    )}
-    {source.location === 'FOOBAR2000' && (
-      <Field placeholder="Accurate rip" name={`sources.${i}.accurateRip`} component={Input} />
-    )}
-    {source.location === 'FOOBAR2000' && (
-      <Field placeholder="Cue issues" name={`sources.${i}.cueIssues`} component={Input} />
-    )}
-    {source.location === 'FOOBAR2000' && <Field placeholder="Discs" name={`sources.${i}.discs`} component={Input} />}
-    {source.location === 'FOOBAR2000' && (
-      <Field placeholder="Download" name={`sources.${i}.download`} component={Input} />
-    )}
-    {source.location === 'FOOBAR2000' && (
-      <Field placeholder="Edition" name={`sources.${i}.edition`} component={Input} />
-    )}
-    {source.location === 'FOOBAR2000' && (
-      <Field placeholder="Format" name={`sources.${i}.format`} component={Select}>
-        {formats.map(format => (
-          <option value={format.id} key={format.id}>
-            {format.label}
+  <Fragment>
+    <FormField label="Location">
+      <Field placeholder="Location" name={`sources.${i}.location`} component={Select}>
+        {locations.map(location => (
+          <option value={location.id} key={location.id}>
+            {location.label}
           </option>
         ))}
       </Field>
+    </FormField>
+    <FormField label="MBID">
+      <Field placeholder="MBID" name={`sources.${i}.mbid`} component={Input} />
+    </FormField>
+    <FormField label="Comments">
+      <Field placeholder="Comments" name={`sources.${i}.comments`} component={Textarea} />
+    </FormField>
+    {source.location !== 'SPOTIFY' && (
+      <FormField label="Tag issues">
+        <Field placeholder="Tag issues" name={`sources.${i}.tagIssues`} component={Input} />
+      </FormField>
     )}
-    <button onClick={() => onRemove(i)}>x</button>
-  </div>
+    {source.location === 'FOOBAR2000' && (
+      <Fragment>
+        <FormField label="Accurate rip">
+          <Field placeholder="Accurate rip" name={`sources.${i}.accurateRip`} component={Input} />
+        </FormField>
+        <FormField label="Cue issues">
+          <Field placeholder="Cue issues" name={`sources.${i}.cueIssues`} component={Input} />
+        </FormField>
+        <FormField label="Discs">
+          <Field placeholder="Discs" name={`sources.${i}.discs`} component={Input} />
+        </FormField>
+        <FormField label="Download">
+          <Field placeholder="Download" name={`sources.${i}.download`} component={Input} />
+        </FormField>
+        <FormField label="Edition">
+          <Field placeholder="Edition" name={`sources.${i}.edition`} component={Input} />
+        </FormField>
+        <FormField label="Format">
+          <Field placeholder="Format" name={`sources.${i}.format`} component={Select}>
+            {formats.map(format => (
+              <option value={format.id} key={format.id}>
+                {format.label}
+              </option>
+            ))}
+          </Field>
+        </FormField>
+      </Fragment>
+    )}
+    <div>
+      <Button onClick={() => onRemove(i)}>x</Button>
+    </div>
+  </Fragment>
 );
--- src/routes/Albums/Album/View.js
+++ src/routes/Albums/Album/View.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { Formik, Field, FieldArray, Form } from 'formik';
 
 import Input from '../../../components/Input';
 import Textarea from '../../../components/Textarea';
 import Source from '../../../components/Source';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 export default ({ data, submit }) => {
   if (data.loading) {
@@ -15,21 +17,27 @@ export default ({ data, submit }) => {
       <Formik initialValues={data.album}>
         {({ isSubmitting, values }) => (
           <Form>
-            <Field placeholder="Title" name="title" component={Input} />
-            <Field placeholder="Artist" name="artist" component={Input} />
-            <Field placeholder="Year" name="year" component={Input} />
-            <Field placeholder="Comments" name="comments" component={Textarea} />
+            <FormField label="Title">
+              <Field placeholder="Title" name="title" component={Input} />
+            </FormField>
+            <FormField label="Artist">
+              <Field placeholder="Artist" name="artist" component={Input} />
+            </FormField>
+            <FormField label="Year">
+              <Field placeholder="Year" name="year" component={Input} />
+            </FormField>
+            <FormField label="Comments">
+              <Field placeholder="Comments" name="comments" component={Textarea} />
+            </FormField>
             <FieldArray
               name="sources"
               render={({ remove, push }) => (
-                <div>
+                <Fragment>
                   {values.sources
                     ? values.sources.map((source, i) => <Source key={i} i={i} source={source} onRemove={remove} />)
                     : null}
-                  <button onClick={() => push({ location: 'APPLE_MUSIC' })} type="button">
-                    Add source
-                  </button>
-                </div>
+                  <Button onClick={() => push({ location: 'APPLE_MUSIC' })}>Add source</Button>
+                </Fragment>
               )}
             />
             <button type="submit" disabled={isSubmitting}>
