ad254709c7c245e9a1b71d5b68b44d7d01980a80
Kirill Korolyov <kirill.korolyov@gmail.com>
2018-03-20 00:09:27 +0000

    More responsive album list layout

--- src/components/Album/styles.js
+++ src/components/Album/styles.js
@@ -8,9 +8,14 @@ export const Anchor = styled(Link)`
   flex-direction: row;
   padding: 5px 10px;
   cursor: pointer;
+  flex-wrap: wrap;
 
   &:hover {
     background-color: #eee;
+  }
+
+  @media (min-width: 600px) {
+    flex-wrap: nowrap;
   }
 `;
 
@@ -25,29 +30,37 @@ export const Column2 = styled.div`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
+  flex-shrink: 1;
+  width: calc(100% - 50px);
+
+  @media (min-width: 600px) {
+    width: auto;
+  }
 `;
 
 export const Column3 = styled.div`
   display: flex;
   flex-direction: column;
-  flex-shrink: 1;
+  flex-shrink: 0;
+  width: 100%;
+  padding-left: 40px;
+  padding-top: 4px;
+
+  @media (min-width: 600px) {
+    width: auto;
+    padding: 9px 0 0 10px;
+  }
 `;
 
 export const Title = styled.div`
   ${large};
   color: #5f5f5f;
   font-weight: 600;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
 `;
 
 export const Artist = styled.div`
   ${base};
   color: #5b5b5b;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
 `;
 
 export const Date = styled.div`
@@ -58,5 +71,7 @@ export const Date = styled.div`
 export const FirstPlayed = styled.div`
   ${small};
   color: #a3a3a3;
-  padding-top: 9px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
 `;
