bc12e5fc5e566cc1fea11b48c9df219a18f19532
Kirill Korolyov <kirill.korolyov@gmail.com>
2019-08-10 17:57:14 +0100

    Migrate Application to hooks

--- src/routes/Application/index.tsx
+++ src/routes/Application/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { compose, withStateHandlers } from 'recompose';
 
 import Index from '../Index';
 import NewAlbum from '../Albums/NewAlbum';
@@ -10,48 +9,45 @@ import Login from '../../components/Login';
 import { Page, Section, Header, H1, GlobalStyles } from './styles';
 import logo from './logo.svg';
 
-const enhance = compose(
-  withStateHandlers(
-    { token: localStorage.getItem('token') },
-    {
-      onLoggedIn: () => token => {
-        localStorage.setItem('token', token);
-        return { token };
-      },
-      onLoggedOut: () => () => {
-        localStorage.removeItem('token');
-        return { token: null };
-      }
-    }
-  )
-);
+export default () => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  const onLoggedIn = useCallback(newToken => {
+    localStorage.setItem('token', newToken);
+    setToken(newToken);
+  }, []);
 
-export default enhance(({ token, onLoggedIn, onLoggedOut }) => (
-  <Router>
-    <GlobalStyles />
-    <Page>
-      <div>
+  const onLoggedOut = useCallback(() => {
+    localStorage.removeItem('token');
+    setToken(null);
+  }, []);
+
+  return (
+    <Router>
+      <GlobalStyles />
+      <Page>
+        <div>
+          <Section>
+            <Header>
+              <img alt="Logo" src={logo} height={48} />
+              <H1>My Music</H1>
+            </Header>
+          </Section>
+          <Section>
+            <Switch>
+              <Route exact path="/" render={() => <Index isLoggedIn={!!token} />} />
+              {token && (
+                <Switch>
+                  <Route exact path="/albums/new" component={NewAlbum} />
+                  <Route exact path="/albums/:id" component={Album} />
+                </Switch>
+              )}
+            </Switch>
+          </Section>
+        </div>
         <Section>
-          <Header>
-            <img alt="Logo" src={logo} height={48} />
-            <H1>My Music</H1>
-          </Header>
+          <Login isLoggedIn={!!token} onLoggedIn={onLoggedIn} onLoggedOut={onLoggedOut} />
         </Section>
-        <Section>
-          <Switch>
-            <Route exact path="/" render={() => <Index isLoggedIn={!!token} />} />
-            {token && (
-              <Switch>
-                <Route exact path="/albums/new" component={NewAlbum} />
-                <Route exact path="/albums/:id" component={Album} />
-              </Switch>
-            )}
-          </Switch>
-        </Section>
-      </div>
-      <Section>
-        <Login isLoggedIn={!!token} onLoggedIn={onLoggedIn} onLoggedOut={onLoggedOut} />
-      </Section>
-    </Page>
-  </Router>
-));
+      </Page>
+    </Router>
+  );
+};
