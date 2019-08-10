5cdb8e00725a62111671d207d47050c33d0fe726
Kirill Korolyov <kirill.korolyov@gmail.com>
2019-08-10 17:39:09 +0100

    Convert Login component to hooks

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,5 @@
-import React, { Fragment } from 'react';
-import { graphql } from '@apollo/react-hoc';
-import { compose, withHandlers, withStateHandlers } from 'recompose';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import { Link } from 'react-router-dom';
 
 import Button from '../../components/Button';
@@ -10,97 +9,76 @@ import Text from '../../components/Text';
 import LoginMutation from './mutation';
 import { Root, Spacer, LoginLink, NewAlbumLink } from './styles';
 
-const enhance = compose(
-  graphql(LoginMutation, {
-    props: ({ mutate }) => ({
-      onLogin: password =>
-        mutate({
-          variables: {
-            password
-          }
-        })
-    })
-  }),
-  withStateHandlers(
-    { loading: false, password: '', showingLogin: false, wrongPassword: false },
-    {
-      cancelLogin: () => () => ({ password: '', showingLogin: false, wrongPassword: false }),
-      showLogin: () => () => ({ showingLogin: true }),
-      setPassword: () => e => ({ password: e.target.value }),
-      loggingStart: () => () => ({ loading: true }),
-      loggingEnd: () => loggedIn => ({
-        loading: false,
-        password: '',
-        showingLogin: !loggedIn,
-        wrongPassword: !loggedIn
-      })
-    }
-  ),
-  withHandlers({
-    login: ({ cancelLogin, loggingStart, loggingEnd, onLoggedIn, password, onLogin }) => async () => {
-      loggingStart();
-      const result = await onLogin(password);
-      const loggedIn = result.data.login;
-      loggingEnd(loggedIn);
-      loggedIn && onLoggedIn(password);
-    },
-    logout: ({ onLoggedOut }) => () => {
-      onLoggedOut();
-    }
-  })
-);
+const Login = ({ isLoggedIn, onLoggedIn, onLoggedOut }) => {
+  const [loginRequest, { loading }] = useMutation(LoginMutation);
 
-const Login = ({
-  cancelLogin,
-  isLoggedIn,
-  loading,
-  login,
-  logout,
-  password,
-  setPassword,
-  showLogin,
-  showingLogin,
-  wrongPassword
-}) => (
-  <Root>
-    {showingLogin ? (
-      <Fragment>
-        <Input
-          autofocus
-          disabled={loading}
-          onChange={setPassword}
-          placeholder={wrongPassword ? 'Sorry, try again' : 'Hey Dremora, please confirm your password'}
-          type="password"
-          value={password}
-        />
-        <Spacer />
-        <Button disabled={loading} onClick={cancelLogin}>
-          Cancel
-        </Button>
-        <Spacer />
-        <Button disabled={loading || !password} onClick={login}>
-          Login
-        </Button>
-      </Fragment>
-    ) : (
-      <Fragment>
-        <LoginLink disabled={loading} onClick={isLoggedIn ? logout : showLogin}>
-          <Text color="lighterGrey" size="small">
-            {loading ? 'Loading…' : isLoggedIn ? 'Log out' : 'Login'}
-          </Text>
-        </LoginLink>
-        {isLoggedIn && (
-          <NewAlbumLink>
-            <Link to="/albums/new">
-              <Text color="lighterGrey" size="small">
-                New album
-              </Text>
-            </Link>
-          </NewAlbumLink>
-        )}
-      </Fragment>
-    )}
-  </Root>
-);
+  const [passwordInput, setPasswordInput] = useState('');
+  const [showingLogin, setShowingLogin] = useState(false);
+  const [wrongPassword, setWrongPassword] = useState(false);
 
-export default enhance(Login);
+  const cancelLogin = () => {
+    setPasswordInput('');
+    setShowingLogin(false);
+    setWrongPassword(false);
+  };
+
+  const showLogin = () => setShowingLogin(true);
+  const setPassword = e => {
+    setWrongPassword(false);
+    setPasswordInput(e.target.value);
+  };
+
+  const login = async () => {
+    const {
+      data: { login: loggedIn }
+    } = await loginRequest({ variables: { password: passwordInput } });
+    setPasswordInput('');
+    setShowingLogin(!loggedIn);
+    setWrongPassword(!loggedIn);
+    loggedIn && onLoggedIn(passwordInput);
+  };
+
+  return (
+    <Root>
+      {showingLogin ? (
+        <>
+          <Input
+            autofocus
+            disabled={loading}
+            onChange={setPassword}
+            placeholder={wrongPassword ? 'Sorry, try again' : 'Hey Dremora, please confirm your password'}
+            type="password"
+            value={passwordInput}
+          />
+          <Spacer />
+          <Button disabled={loading} onClick={cancelLogin}>
+            Cancel
+          </Button>
+          <Spacer />
+          <Button disabled={loading || !passwordInput} onClick={login}>
+            Login
+          </Button>
+        </>
+      ) : (
+        <>
+          <LoginLink disabled={loading} onClick={isLoggedIn ? onLoggedOut : showLogin}>
+            <Text color="lighterGrey" size="small">
+              {loading ? 'Loading…' : isLoggedIn ? 'Log out' : 'Login'}
+            </Text>
+          </LoginLink>
+          {isLoggedIn && (
+            <NewAlbumLink>
+              <Link to="/albums/new">
+                <Text color="lighterGrey" size="small">
+                  New album
+                </Text>
+              </Link>
+            </NewAlbumLink>
+          )}
+        </>
+      )}
+    </Root>
+  );
+};
+
+export default Login;
