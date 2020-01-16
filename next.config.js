083df4d2a09fc6118b266a0d4988c95c9e6cc443
Kirill Korolyov <kirill.korolyov@gmail.com>
2020-01-16 03:36:40 +0000

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
+const bundleAnalyzer = require('@next/bundle-analyzer');
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-module.exports = phase => {
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer(phase => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
   const isProd = phase === PHASE_PRODUCTION_BUILD;
 
   const env = {
     API_URL: (() => {
       if (isDev) return 'http://localhost:4000';
+
       if (isProd) {
         return 'https://my-music-api.dremora.com';
       }
@@ -14,4 +20,4 @@ module.exports = phase => {
   };
 
   return { env };
-};
+});
